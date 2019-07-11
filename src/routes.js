c5e158e32b88dfb9c2bae3abb224e583e2d1d525
Jhonatan I <jhonatan-nascimento94@hotmail.com>
2019-07-10 21:14:07 -0300

    JWT

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,10 @@
 import { Router } from 'express';
+
 import UserController from './app/controllers/UserController';
+import SessionsController from './app/controllers/SessionController';
 
 const routes = new Router();
 routes.post('/users', UserController.store);
+routes.post('/sessions', SessionsController.store);
 
 export default routes;
