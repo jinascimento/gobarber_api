e14f6c1040f7403a76d2cebc16f08438a8e12eb4
Jhonatan I <jhonatan-nascimento94@hotmail.com>
2019-07-17 13:20:41 -0300

    Cancelation appointments

--- src/routes.js
+++ src/routes.js
@@ -22,8 +22,11 @@ routes.put('/users', UserController.update);
 routes.get('/providers', ProviderController.index);
 routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
+routes.delete('/appointments/:id', AppointmentController.delete);
+
 routes.post('/files', upload.single('file'), FileController.store);
 routes.get('/schedule', ScheduleController.index);
+
 routes.get('/notifications', NotificationController.index);
 routes.put('/notifications/:id', NotificationController.update);
 
