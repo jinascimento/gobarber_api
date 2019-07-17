3160534b852dd7618dc049f1b96aeb11d287b1f3
Jhonatan I <jhonatan-nascimento94@hotmail.com>
2019-07-17 00:00:58 -0300

    List notifications and fix appointments create

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,9 +1,10 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore } from 'date-fns';
-
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 import Appointment from '../models/Appointments';
 import User from '../models/User';
 import File from '../models/File';
+import Notification from '../schemas/Notification';
 
 class AppointmentController {
   async index(req, res) {
@@ -54,6 +55,10 @@ class AppointmentController {
       return res.status(401).json({ error: 'You can only create appointments whit providers' });
     }
 
+    if (req.userId === provider_id) {
+      return res.status(401).json({ error: 'You cant create appointments with yourself' });
+    }
+
     const hourStart = startOfHour(parseISO(date));
 
     // Check for past dates
@@ -75,6 +80,19 @@ class AppointmentController {
       provider_id,
       date,
     });
+
+    const user = await User.findByPk(req.userId);
+    const formattedDate = format(
+      hourStart,
+      "'dia' dd 'de' MMMM', ás' H:mm'h'",
+      { locale: pt },
+    );
+
+    await Notification.create({
+      content: `Novo agendamento de ${user.name} para ${formattedDate}`,
+      user: provider_id,
+    });
+
     return res.json(appointment);
   }
 }
