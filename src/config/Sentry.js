9b733006869606e5c11f761f8f3eb955f615d27e
Jhonatan I <jhonatan-nascimento94@hotmail.com>
2019-07-17 19:46:13 -0300

    Environments variable

--- src/config/Sentry.js
+++ src/config/Sentry.js
@@ -1,3 +1,3 @@
 export default {
-  dsn: 'https://4f48eb1cd61b407b8348ff3b06c683b1@sentry.io/1507511',
-}
+  dsn: process.env.SENTRY_DSN,
+};
--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: '2525',
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: 'a00c745e824df7',
-    pass: 'd4af86047d6be3',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe de suporte <noreply@gobarber.com.br>',
